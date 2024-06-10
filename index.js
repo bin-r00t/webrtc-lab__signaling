12f6d856ada524f7f898449af17f756c2d566c05
liubin <bh0bama@sina.com>
2024-06-11 00:03:10 +0800

    update https

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const express = require("express");
 const { Server } = require("socket.io");
 const app = express();
 const setup = require("./setup");
 
-const expressServer = http.createServer(app);
+const expressServer = https.createServer({
+  key: fs.readFileSync("./certs/private.key"),
+  cert: fs.readFileSync("./certs/cert.pem"),
+  passphrase: '06092024hz'
+}, app);
 const socketServer = new Server(expressServer, {
   maxHttpBufferSize: 1048576, // 默认1e6, 即1000000, 不是严格的1MB, 此处限制载荷为1MB
   cors: {
@@ -13,6 +18,6 @@ const socketServer = new Server(expressServer, {
 });
 setup(socketServer);
 
-expressServer.listen(8000, () => {
+expressServer.listen(8000, "0.0.0.0", () => {
   console.log("Server is running on port 8000");
 });
